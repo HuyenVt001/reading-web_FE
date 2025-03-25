6b99e9385146fe2a7431e2919ad21d4fec0be0c0
vth <thanhhuyenvo00@gmail.com>
2025-03-25 07:23:47 +0700

    update

--- src/components/MainDivider.jsx
+++ src/components/MainDivider.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import StarBackgound from './background/StarBackground';
 import Header from './Header';
 
 function MainDivider() {
